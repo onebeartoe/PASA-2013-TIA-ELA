3cc13454a31442f5fe5b495e1959f44823954772
onebeartoe <onebeartoe@gmail.com>
2013-10-05 19:16:58 -0500

    The init daemon for the Alameda server was added.  The sketch for the horizontal EL wires was added.

---
+++ alameda/output/horizontal/horizontal_el_wire_sequencer/horizontal_el_wire_sequencer.ino
@@ -0,0 +1,86 @@
+
+
+#define MODES_ALL_ON 1
+#define MODES_STEP_THROUGH 2
+
+#define A  2
+#define B  3
+#define C  4
+#define D  5
+#define E  6
+#define FF  7
+#define G  8
+#define H  9
+
+int currentMode = MODES_ALL_ON;
+
+void loop()
+{ 
+  if (Serial.available())
+  {
+    currentMode = Serial.read() - '0';
+  }
+  
+  switch(currentMode)
+  {
+    case MODES_ALL_ON:
+    {
+      allOn();
+      break;
+    }
+    case MODES_STEP_THROUGH:
+    {
+      stepThrough();
+      break;
+    }
+    default:
+    {
+      allOn();
+    }
+  }
+}
+
+void allOn()
+{
+
+     digitalWrite(A, HIGH); 
+     digitalWrite(B, HIGH); 
+     digitalWrite(C, HIGH); 
+     digitalWrite(D, HIGH); 
+     digitalWrite(E, HIGH); 
+     digitalWrite(FF, HIGH); 
+     digitalWrite(G, HIGH); 
+     digitalWrite(H, HIGH);    
+}
+
+void setup() 
+{
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(FF, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(H, OUTPUT);
+  
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+void stepThrough()
+{
+  int x,status;
+  
+  // Step through all eight EL channels (pins 2 through 9)
+  for (x=2; x<=9; x++)
+  {
+    digitalWrite(x, HIGH);   // turn the EL channel on
+    delay(100);              // wait for 1/10 second
+    digitalWrite(x, LOW);    // turn the EL channel off
+
+    digitalWrite(10, status);   // blink both status LEDs
+    digitalWrite(13, status);
+    status = !status; 
+  }  
+}
