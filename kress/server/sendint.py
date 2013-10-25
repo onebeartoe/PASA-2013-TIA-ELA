988b960a56bda66d8cd1c368b5d9b5321187f416
onebeartoe <onebeartoe@gmail.com>
2013-10-25 11:56:33 -0500

    More exception handling was added.

--- kress/server/sendint.py
+++ kress/server/sendint.py
@@ -1,4 +1,5 @@
 import serial;
+import serial.serialutil;
 
 def sendInt(ser, i):
 	
@@ -10,7 +11,7 @@ def sendInt(ser, i):
 			ser = serial.Serial('/dev/ttyACM0', 9600);
 			time.sleep(2.0);
 			ser.open();			
-		except OSError:
+		except OSError, SerialException:
 			print "could not open the serial port"
 	
 	if ser is not None:
