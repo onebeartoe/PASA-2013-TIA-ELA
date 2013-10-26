758c4d3ebc281ac5e53ff632aa5a939f8232aea9
onebeartoe <onebeartoe@gmail.com>
2013-10-25 22:43:53 -0500

    Some clean up was done and basic functionallity was added.

--- kress/arduino_relay_and_window_lights/arduino_relay_and_window_lights.ino
+++ kress/arduino_relay_and_window_lights/arduino_relay_and_window_lights.ino
@@ -7,23 +7,58 @@
 
 // light modes
 #define MODES_ALL_ON 1
-#define MODES_MOTION_DETECTED_A 2
-#define MODES_RANDOM 3
+#define MODES_RANDOM 2
 #define MODES_LADDER 4
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+//int currentMode = 172543;
 int currentMode = MODES_ALL_ON;
 
 int motionDetectedAHeight = 0;
 
 int spireSequencerPin = 9;  // connected digital pin 9
 
+// remove after testing
+const int ledPin =  13;      // the number of the LED pin
+const int ledPin2 = 12;
+int ledState = LOW;             // ledState used to set the LED
+long previousMillis = 0;        // will store last time LED was updated
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 1000;           // interval at which to blink (milliseconds)
+// end of test varialbes
+
+
 int leftLightPin = 10;
 
 int rightLightPin = 11;
 
 int subladderMode = 0;
+
+void blink()
+{
+  // check to see if it's time to blink the LED; that is, if the 
+  // difference between the current time and last time you blinked 
+  // the LED is bigger than the interval at which you want to 
+  // blink the LED.
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis > interval) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+    digitalWrite(ledPin2, ledState);
+  }  
+}
 
 void elLadder()
 {
@@ -84,22 +119,16 @@ void loop()
       digitalWrite(leftLightPin, HIGH);
       digitalWrite(rightLightPin, HIGH); 
 
-      neoPixelsLadder(N_PIXELS, 500);
-      
-      break;
-    }
-    case MODES_MOTION_DETECTED_A:
-    {
-      analogWrite(spireSequencerPin, 1023);  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
-      
-      motionDetected();
+digitalWrite(ledPin, HIGH);
+digitalWrite(ledPin2, HIGH);
       
       break;
     }
     case MODES_RANDOM:
     {
-      analogWrite(spireSequencerPin, 300);
-      randomly();
+      analogWrite(spireSequencerPin, 20);
+blink();
+//      randomly();
       
       break;
     }
@@ -111,64 +140,12 @@ void loop()
     }    
     default:
     {
-      neoPixelsLadder(3, 500);      
+      blink();
     }
   }
 }
 
 boolean yellowLights = true;
-
-void motionDetected()
-{
-  motionDetectedAHeight++;
-  
-  if(motionDetectedAHeight > N_PIXELS)
-  {
-    motionDetectedAHeight = 0;
-  }
-  
-  neoPixelsLadder(motionDetectedAHeight, 20);
-  
-  if(yellowLights)
-  {
-    digitalWrite(leftLightPin, HIGH);
-    digitalWrite(rightLightPin, HIGH); 
-  }
-  else
-  {
-    digitalWrite(leftLightPin, LOW);
-    digitalWrite(rightLightPin, LOW); 
-  }
-  yellowLights != yellowLights;
-}
-
-void neoPixelsLadder(int height, long msDelay)
-{
-  uint8_t  i;
-
-  if(height < 0)
-  {
-    height = 0;
-  }
-  
-  if(height > N_PIXELS)
-  {
-    height = N_PIXELS;
-  }
-
-  // Color pixels based on rainbow gradient
-  for(i=0; i<N_PIXELS; i++) 
-  {
-    if(i >= height)               
-      strip.setPixelColor(i,   0,   0, 0);
-    else 
-      strip.setPixelColor(i,Wheel(map(i,0,strip.numPixels()-1,30,150)));    
-  }
- 
-  delay(msDelay);
-  
-   strip.show(); // Update strip
-}
 
 void randomly()
 {
@@ -182,6 +159,10 @@ void randomly()
 
 void setup() 
 {
+  // dummy pin for on-board LED
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  
   pinMode (leftLightPin,OUTPUT);
   
   pinMode (rightLightPin,OUTPUT);
@@ -209,5 +190,4 @@ uint32_t Wheel(byte WheelPos)
    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
-
 
